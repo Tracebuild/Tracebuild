2c7edf8f46d806ec3b133e76bffb0a5a4b1b6568
Jonas Jud <jonas.jud87@gmail.com>
2026-06-30 00:01:19 +0200

    changed names and EST Lint config

--- backend/workers/tasks.py
+++ backend/workers/tasks.py
@@ -2,7 +2,7 @@ from celery import Celery
 from app.core.config import settings
 
 celery_app = Celery(
-    "construction_analyzer",
+    "tracebuild",
     broker=settings.redis_url,
     backend=settings.redis_url,
 )
